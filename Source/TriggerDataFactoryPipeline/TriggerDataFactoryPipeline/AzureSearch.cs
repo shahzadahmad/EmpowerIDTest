67481e092de75262861373f3056a0507cf1873d9
shahzadnetlink <shahzad@netlinkcontrol.com>
2023-09-15 03:35:11 +0500

    Added Missing File functionality into Git Repository.

---
+++ Source/TriggerDataFactoryPipeline/TriggerDataFactoryPipeline/AzureSearch.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using Azure.Search.Documents;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TriggerDataFactoryPipeline
+{
+    public class AzureSearch
+    {
+        public async Task<IEnumerable<string>> Search(string query)
+        {
+            SearchClient searchClient = CreateSearchClientForQueries(ConfigurationManager.AppSettings["searchIndexName"]);
+            SearchOptions options = new SearchOptions()
+            {
+                IncludeTotalCount = true,
+                //Filter = "",
+                //Size = pageSize,
+                //Skip = (page - 1) * pageSize
+            };
+            var results = await searchClient.SearchAsync<Product>(query, options);
+
+            List<string> documents = new List<string>();
+            Console.WriteLine(results.Value.TotalCount);
+            foreach (var s in results.Value.GetResults())
+            {
+                documents.Add(JsonSerializer.Serialize<Product>(s.Document));
+            }
+            return documents;
+        }
+
+        private static SearchClient CreateSearchClientForQueries(string indexName)
+        {
+            string searchServiceEndPoint = ConfigurationManager.AppSettings["searchServiceEndPoint"];
+            string queryApiKey = ConfigurationManager.AppSettings["searchQueryApiKey"];
+
+            SearchClient searchClient = new SearchClient(new Uri(searchServiceEndPoint), indexName, new AzureKeyCredential(queryApiKey));
+            return searchClient;
+        }
+    }
+}
---
+++ Source/TriggerDataFactoryPipeline/TriggerDataFactoryPipeline/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriggerDataFactoryPipeline
+{    
+    public class Product
+    {
+        public string product_id { get; set; }
+        public int category_id { get; set; }
+        public string product_name { get; set; }
+        public string category { get; set; }
+        public string price { get; set; }
+        public string description { get; set; }
+        public string date_added { get; set; }
+    }
+}
