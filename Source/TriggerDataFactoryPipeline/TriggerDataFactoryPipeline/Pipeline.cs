02206b0bdec1a0312697fa90f6459a2108e3b41f
shahzadnetlink <shahzad@netlinkcontrol.com>
2023-09-15 01:10:07 +0500

    Updated Application Code

--- Source/TriggerDataFactoryPipeline/TriggerDataFactoryPipeline/Pipeline.cs
+++ Source/TriggerDataFactoryPipeline/TriggerDataFactoryPipeline/Pipeline.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Management.DataFactory.Models;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Microsoft.Rest;
 using System;
@@ -10,20 +11,20 @@ namespace TriggerDataFactoryPipeline
         public IDataFactoryManagementClient client;
 
         private string applicationId = "4d0ff30d-bcdd-449b-9eeb-c06d93fe8961";
-        private string clientSecret = "uDd8Q~J_h74DwFnao70aA5ZT.hjMkJGzw28uCcKb";
+        private string clientSecret = "nvN8Q~SByqgEVlJuskc2gJdNUKyJLtOHACGMyboc";
         private string subscriptionId = "b19f4ee4-a01e-4706-8a8c-96558113ac54";
         private string tenantID = "1363ebc4-b673-4c1d-b408-71fb8624a0d1";
         private readonly string activeDirectoryEndpoint = "https://login.windows.net/";
-        private readonly string windowsManagementUri = "https://management.azure.com/";
+        private readonly string windowsManagementUri = "https://management.core.windows.net/";
 
         public void create_adf_client()
         {
             // Authenticate and create a data factory management client
             var authority = new Uri(new Uri(activeDirectoryEndpoint), this.tenantID);
-            var context = new AuthenticationContext(authority.AbsoluteUri);
-            var ClientCredential = new ClientCredential(applicationId, clientSecret);
+            var authenticationContext = new AuthenticationContext(authority.AbsoluteUri);
+            var clientCredential = new ClientCredential(applicationId, clientSecret);
             
-            var result = context.AcquireTokenAsync(windowsManagementUri, ClientCredential).Result;
+            var result = authenticationContext.AcquireTokenAsync(windowsManagementUri, clientCredential).Result;            
 
             var cred = new TokenCredentials(result.AccessToken);
             client = new DataFactoryManagementClient(cred)
@@ -39,11 +40,44 @@ namespace TriggerDataFactoryPipeline
 
         public void StartPipeline(string resourceGroup, string dataFactoryName, string pipelineName)
         {
-            Console.WriteLine("Creating pipeline run...");
+            try
+            {
+                // Create a Pipeline Run  
+                Console.WriteLine("Creating pipeline run...");
+                var runResponse = client.Pipelines.CreateRunWithHttpMessagesAsync(resourceGroup, dataFactoryName, pipelineName).Result.Body;
+                Console.WriteLine("Pipeline run ID: " + runResponse.RunId);
 
-            var runResponse = client.Pipelines.CreateRunWithHttpMessagesAsync(resourceGroup, dataFactoryName, pipelineName).Result.Body;
+                // Monitor the Pipeline Run  
+                Console.WriteLine("Checking Pipeline Run Status...");
+                PipelineRun pipelineRun;
+                while (true)
+                {
+                    pipelineRun = client.PipelineRuns.Get(resourceGroup, dataFactoryName, runResponse.RunId);
+                    Console.WriteLine("Status: " + pipelineRun.Status);
+                    if (pipelineRun.Status == "InProgress")
+                        System.Threading.Thread.Sleep(15000);
+                    else
+                        break;
+                }
 
-            Console.WriteLine("Pipeline run ID: " + runResponse.RunId);
+                // Check the Copy Activity Run Details  
+                Console.WriteLine("Checking copy activity run details...");
+                if (pipelineRun.Status == "Succeeded")
+                {
+                    Console.WriteLine("Copy Activity Succeeded!");
+                }
+                else
+                {
+                    Console.WriteLine("Copy Activity Failed!");
+                }
+                Console.WriteLine("\nPress any key to exit...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
         }
     }
 }
